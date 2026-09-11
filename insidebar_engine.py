a40f04a5531179d17d8ab76fb82af5acf3a2d7bb
Cassio Carvalho <cassiocarvalho750@gamil.com>
2026-09-11 16:12:19 -0300

    Insidebar: K=3, afrouxa contracao (0.95) e compressao (0.80) p/ mais sinais

--- insidebar_engine.py
+++ insidebar_engine.py
@@ -30,10 +30,10 @@ CONS_MIN       = 3
 CONS_MAX       = 10
 CONS_AMPL_ATR  = 2.5
 CONS_PEN_EMA   = 0.98
-ATR_CONTR_MAX  = 0.90    # afrouxado de 0.80 (menos exigente na contracao)
+ATR_CONTR_MAX  = 0.95    # afrouxado (menos exigente na contracao; +sinais)
 ATR_FAST       = 5
 ATR_SLOW       = 20
-IB_COMPRESS    = 0.70
+IB_COMPRESS    = 0.80    # afrouxado de 0.70 (IB ate 80% da mae; +sinais)
 IB_PERTO_EMA   = 0.05    # afrouxado de 0.03 (IB ate 5% da EMA20)
 
 
